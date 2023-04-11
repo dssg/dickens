71829bce18cec34e01de1eaa950ba57e75161306
Jesse London <jesselondon@gmail.com>
2023-04-11 13:57:16 -0500

    support use of `cachedproperty` as a non-decorating descriptor, *etc.*
    * implements `__set_name__` to support use of `cachedproperty` as a
      class member *without* decoration of a purpose-defined self-
      descriptive method (and other edge cases)
    
    * adds class-only constructor `cachedproperty.static` to wrap callables
      which do not require nor expect any arguments (*i.e.* not even the
      bound instance `self`)
    
    * adds requirement that bound instance must define a typical `__dict__`
      (no `__slots__`, *etc.*)
    
    * implements `cachedproperty.__class_getitem__` for use of class in type
      annotations
    
    * `cachedproperty` instance attribute `__doc__` now defined to carry
      over this value from the wrapped callable
    
    resolves #4

--- src/descriptors.py
+++ src/descriptors.py
@@ -3,27 +3,11 @@
 Additional decorators implementing the descriptor interface.
 
 """
-__version__ = '2.0.0'
+import functools
+import types
 
 
-class cachedproperty:
-    """Non-data descriptor decorator implementing a read-only property
-    which overrides itself on the instance with an entry in the
-    instance's data dictionary, caching the result of the decorated
-    property method.
-
-    """
-    def __init__(self, func):
-        self.__func__ = func
-
-    def __get__(self, instance, _type=None):
-        if instance is None:
-            return self
-
-        setattr(instance, self.__func__.__name__, self.__func__(instance))
-
-        # This descriptor is now overridden for this instance:
-        return getattr(instance, self.__func__.__name__)
+__version__ = '2.1.0'
 
 
 class classproperty:
@@ -98,3 +82,96 @@ class classonlymethod:
             f"(method '{self.__func__.__qualname__}' is accessible "
             f"from '{owner.__name__}' class only)"
         )
+
+
+class cachedproperty:
+    """Non-data descriptor decorator implementing a read-only property
+    which overrides itself on the instance with an entry in the
+    instance's data dictionary, caching the result of the decorated
+    property method.
+
+    For example:
+
+        class ClassyClass:
+
+            @cachedproperty
+            def once_per_instance(self):
+                return self.a + self.b
+
+            @cachedproperty.static
+            def o_n_c_e__per_instance():
+                return 2 ** 1_000
+
+            once__p_e_r__instance = cachedproperty.static(expensive_func)
+
+    The instance's cache may be cleared by simply deleting the instance
+    attribute --
+
+        classy = ClassyClass()
+
+        classy.o_n_c_e__per_instance
+
+        delattr(classy, 'o_n_c_e__per_instance')
+
+    -- or by directly manipulating the instance's __dict__:
+
+        del classy.__dict__['o_n_c_e__per_instance']
+
+    """
+    @classonlymethod
+    def static(cls, func):
+        @functools.wraps(func)
+        def wrapper(_instance):
+            return func()
+
+        return cls(wrapper)
+
+    def __init__(self, func):
+        self.__func__ = func
+        self.__doc__ = func.__doc__
+        self.name = None
+
+    def __set_name__(self, owner, name):
+        if self.name is None:
+            self.name = name
+            return
+
+        if self.name != name:
+            raise TypeError(
+                f"cannot assign the same {self.__class__.__name__} to two different "
+                f"names on {owner.__name__} ({self.name!r} and {name!r})"
+            )
+
+    @property
+    def __name__(self):
+        if self.name is None:
+            return self.__func__.__name__
+
+        return self.name
+
+    def __get__(self, instance, _owner=None):
+        if instance is None:
+            return self
+
+        try:
+            cache = instance.__dict__
+        except AttributeError:
+            # not all objects have __dict__ (e.g. class defines slots)
+            raise TypeError(
+                f"instance of type {instance.__class__.__name__} does not define "
+                f"'__dict__' with which to cache property {self.__name__}"
+            ) from None
+
+        value = self.__func__(instance)
+
+        try:
+            cache[self.__name__] = value
+        except TypeError:
+            raise TypeError(
+                f"'__dict__' of instance of type {instance.__class__.__name__} does "
+                f"not support item assignment for cache of property {self.__name__}"
+            ) from None
+
+        return value
+
+    __class_getitem__ = classmethod(types.GenericAlias)
